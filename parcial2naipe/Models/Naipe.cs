0d8e19cf19cf80f49ed65fbd52e637044a00ba01
Sergio Loza <a2020110121@estudiantes.upsa.edu.bo>
2021-10-13 17:51:52 -0400

    Clase Naipes añadida

---
+++ parcial2naipe/Models/Naipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parcial2naipe.Models
+{
+    public class Naipe
+    {
+
+
+        [Key]
+        [Display(Name = "ID del naipe")]
+        public int IdCancion { get; set; }
+
+
+
+        [Required] //primero las condiciones
+        [Display(Name = "Nombre de Carta")]
+        public string NombreCarta { get; set; }
+
+
+        [Url] 
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Ingrese Enlace de la cancion")]
+        [Display(Name = "Enlace")]
+        public string EnlaceCarta { get; set; }
+    }
+}
