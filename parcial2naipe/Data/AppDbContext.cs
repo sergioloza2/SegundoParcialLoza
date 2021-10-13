33c55e9cbfa04ac613b99d4dd65a79c48d8e3ced
Sergio Loza <a2020110121@estudiantes.upsa.edu.bo>
2021-10-13 17:58:25 -0400

    Agregado AppDbContext

---
+++ parcial2naipe/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using parcial2naipe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parcial2naipe.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Naipe> Naipe { get; set; }
+    }
+}
--- parcial2naipe/Startup.cs
+++ parcial2naipe/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using parcial2naipe.Data;
 
 namespace parcial2naipe
 {
@@ -26,6 +28,9 @@ namespace parcial2naipe
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
